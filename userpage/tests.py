f6afe082b7e44eeed19855f75e9c1dab5aa87c79
unknown <yf-xu16@mails.tsinghua.edu.cn>
2018-12-23 16:35:13 +0800

    Update tests.py

--- userpage/tests.py
+++ userpage/tests.py
@@ -1555,7 +1555,7 @@ class GetNotificationsTest(TestCase):
         self.assertEqual(right, 1)
         avatar_url = "https://763850.iterator-traits.com/showimage/home/ubuntu/QingXian/media/picture/test.png"
         self.assertEqual(notice["user_avatar_url"],avatar_url)
-        self.assertEqual(notice["task_image_url"], "")
+        self.assertEqual(notice["task_image_url"], "https://763850.iterator-traits.com/showimage/home/ubuntu/QingXian/media/picture/default_image.png")
 
         Picture.objects.create(picture_url=str(SITE_DOMAIN).rstrip("/") + "/showimage/home/ubuntu/QingXian/media/picture/test.png",
                                task_id=task.id,
